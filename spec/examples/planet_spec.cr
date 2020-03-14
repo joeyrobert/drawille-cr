62f0f9a94da9951e7391a5ccf924622cbf832920
joeyrobert <joey@joeyrobert.org>
2020-03-14 19:00:21 -0400

    Planet example: Add reverse to spec

--- spec/examples/planet_spec.cr
+++ spec/examples/planet_spec.cr
@@ -9,7 +9,7 @@ describe PlanetExample do
       (40...80).each do |terminal_columns|
         it "renders planet in #{terminal_columns}x#{terminal_lines}=#{terminal_columns*terminal_lines} terminal" do
           c = Drawille::Canvas.new(terminal_lines, terminal_columns)
-          planet_example.draw_frame(c, planet_texture, 0, 10)
+          planet_example.draw_frame(c, planet_texture, 0, 10, false)
         end
       end
     end
